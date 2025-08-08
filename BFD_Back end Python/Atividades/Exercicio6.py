d33ca62bbd7311caa7b43edf819dbbfe7563cbb1
CaioDevFlutter <jv.0602.fs@gmail.com>
2025-08-08 11:56:24 -0300

    Exercicios-BFD

---
+++ BFD_Back end Python/Atividades/Exercicio6.py
@@ -0,0 +1,6 @@
+palavra1 = input("Digite uma palavra: ")
+palavra2 = input("Digite mais uma palavra: ")
+palavra3 = input("Digite outra palavra: ")
+palavra4 = input("Digite uma ultima palavra: ")
+
+print(palavra1,palavra2,palavra3,palavra4)
---
+++ BFD_Back end Python/Atividades/Exercicio7.py
@@ -0,0 +1,4 @@
+CIDADE_NASC = input ("Em que cidade você nasceu? ")
+ESTADO_NASC = input ("Em que estado fica esta cidade? ")
+
+print(F"Você nasceu em{CIDADE_NASC},{ESTADO_NASC} ")
