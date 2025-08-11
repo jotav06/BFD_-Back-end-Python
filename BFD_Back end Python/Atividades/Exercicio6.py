868653ce52e75d72c576832bedb314c34550a7dc
jotav06 <jv.0602.fs@gmail.com>
2025-08-11 15:35:38 -0300

    Update Exercicio6.py

--- BFD_Back end Python/Atividades/Exercicio6.py
+++ BFD_Back end Python/Atividades/Exercicio6.py
@@ -1,6 +1,6 @@
-palavra1 = input("Digite uma palavra: ")
-palavra2 = input("Digite mais uma palavra: ")
-palavra3 = input("Digite outra palavra: ")
-palavra4 = input("Digite uma ultima palavra: ")
+palavra_1 = input("Digite uma palavra: ")
+palavra_2 = input("Digite mais uma palavra: ")
+palavra_3 = input("Digite outra palavra: ")
+palavra_4 = input("Digite uma ultima palavra: ")
 
-print(palavra1,palavra2,palavra3,palavra4)
+print(palavra_1,palavra_2,palavra_3,palavra_4)
