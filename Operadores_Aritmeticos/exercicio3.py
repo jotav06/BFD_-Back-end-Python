eb5d895811c1fa2fe340c26d4f0f8bb4884bacc9
jotav06 <jv.0602.fs@gmail.com>
2025-08-13 16:54:16 -0300

    Update exercicio3.py

---
+++ Operadores_Aritmeticos/exercicio3.py
@@ -0,0 +1,7 @@
+idade_usuario = int(input("Quantos anos você tem? "))
+IDADE_MINIMA = 18
+
+if idade_usuario >= IDADE_MINIMA:
+    print("você é maior de idade")
+else:
+    print("Você é menor de idade")
